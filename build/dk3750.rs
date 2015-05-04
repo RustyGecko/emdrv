f0ca03ea8517d05de2929fca7f25c6e2b8262f4d
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2015-05-04 18:04:29 +0200

    Added ssd2119_direct depedency

--- build/dk3750.rs
+++ build/dk3750.rs
@@ -6,6 +6,9 @@ pub fn kit_config(config: &mut Config) -> &mut Config {
         .include("efm32-common/kits/EFM32GG_DK3750/config")
 
         .include("efm32-common/reptile/glib")
+
+        .file("efm32-common/reptile/glib/dmd/ssd2119/dmd_ssd2119_direct.c")
         .file("efm32-common/kits/common/drivers/tftdirect.c")
         .file("efm32-common/kits/common/drivers/tftspi.c")
+
 }
